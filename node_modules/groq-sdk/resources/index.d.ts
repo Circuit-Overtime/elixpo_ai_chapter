7f93deb1868755b41176e269f58e7dcad15a692c
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2024-10-15 20:53:14 +0530

    Update Readme

--- node_modules/groq-sdk/resources/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "./shared.js";
-export { Audio } from "./audio/audio.js";
-export { Chat } from "./chat/chat.js";
-export { CompletionUsage, Completions } from "./completions.js";
-export { CreateEmbeddingResponse, Embedding, EmbeddingCreateParams, Embeddings } from "./embeddings.js";
-export { Model, ModelDeleted, ModelListResponse, Models } from "./models.js";
-//# sourceMappingURL=index.d.ts.map
